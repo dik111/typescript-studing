074f64a3905e93072ca71b7dcc204db12fbe9cb1
yuweiZhan <195812208@qq.com>
2020-10-07 13:54:59 +0800

    :sparkles: 定义静态类型的变量

---
+++ demo2.ts
@@ -0,0 +1,13 @@
+let count: number = 1
+
+interface xiaojiejie {
+    uname: string,
+    age: number
+}
+
+const xiaohong: xiaojiejie = {
+    uname: 'xiaohong',
+    age: 18
+}
+
+console.log(xiaohong)
