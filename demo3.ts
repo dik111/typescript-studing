4dc26eb56d3b351a27527d0312cc19a915e7f359
yuweiZhan <195812208@qq.com>
2020-10-07 14:05:28 +0800

    :sparkles: 基础静态类型和对象静态类型

---
+++ demo3.ts
@@ -0,0 +1,16 @@
+
+const count : number = 918
+const myName : string = 'xiaoming'
+
+const xiaojiejie:{
+    name:string,
+    age:number
+} = {
+    name:'dajiao',
+    age:18
+}
+
+const xiaojiejies :string[] = ['xiaohong','xiaohong2','xiaohong3']
+
+class Person{}
+const  dajiao:Person = new Person()
